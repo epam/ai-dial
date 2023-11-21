89aaf01f7a049b7ef9ff45a17fe8c5763ad046c4
dspashynskyi <dmytro_pashynskyi@epam.com>
2023-11-21 18:19:17 +0200

    changing baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://docs.epam-rail.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ai-dial/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -24,7 +24,7 @@ const config = {
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',//'throw', for exeptions
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
