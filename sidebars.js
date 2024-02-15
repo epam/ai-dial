8cd723c69c269ecdc2794823d6ce2b59b80acf1c
sr-remsha <46220620+sr-remsha@users.noreply.github.com>
2024-02-15 12:48:28 +0100

    fix in sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -49,7 +49,7 @@ const sidebars = {
           label: 'Helm Deployment',
           href: 'https://github.com/epam/ai-dial-helm/tree/main/charts/dial/examples/generic/simple',
         },
-        'Deployment/configuration',
+        'Deployment/Configuration',
         'Deployment/OpenAI Model Deployment',
         'Deployment/Vertex Model Deployment',
         'Deployment/Bedrock Model Deployment'
