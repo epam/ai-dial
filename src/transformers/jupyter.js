a4fdf46ce8bdf7dff36df532c66832f3c2cc8163
Aliaksei Kryvasheyeu <aliaksei_kryvasheyeu@epam.com>
2024-08-08 12:44:33 +0200

    feat: adds remark transformer for jupyter

---
+++ src/transformers/jupyter.js
@@ -0,0 +1,17 @@
+const regexp = /.ipynb$/i;
+
+/** @type {import('@remark-embedder/core').Transformer} */
+const jupyterTransformer = {
+  name: 'jupyterTransformer',
+  shouldTransform: async (url) => {
+    return url && regexp.test(url);
+  },
+  getHTML: async (urlString, getConfig = {}) => {
+    console.log(urlString);
+    return `<div class="jupyter">
+        <a href=${urlString} target="_blank" rel="noopener noreferrer"><img class="no-zoom" src="/jupyter.svg" alt="Jupyter" /> View Jupyter Notebook</a>
+    </div>`;
+  },
+};
+
+export default jupyterTransformer;
